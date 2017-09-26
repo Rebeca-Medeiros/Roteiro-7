b4d1f6319f7c79c867c4dba34c7bb8aa6039fd39
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-09-26 11:29:12 -0300

    Update FiguraGeometrica.h

--- Questao 1/FiguraGeometrica.h
+++ Questao 1/FiguraGeometrica.h
@@ -1,1 +1,16 @@
+#ifndef FIGURAGEOMETRICA_H
+#define FIGURAGEOMETRICA_H
+#include <iostream>
+#include <string>
 
+
+class FiguraGeometrica{
+	private:
+		std::string nome;
+	public:
+		FiguraGeometrica(std::string nome);
+		float calcularArea();		
+};
+
+
+#endif // FIGURAGEOMETRICA_H
