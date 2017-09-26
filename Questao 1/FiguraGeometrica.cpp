5edfdd4b4404d7b5264dcc42a3a2eecce6b8a04b
Rebeca Medeiros <rebeca.andrade.medeiros@gmail.com>
2017-09-26 11:31:09 -0300

    Add files via upload

---
+++ Questao 1/FiguraGeometrica.cpp
@@ -0,0 +1,10 @@
+#include "FiguraGeometrica.h"
+#include <iostream>
+
+using namespace std;
+
+FiguraGeometrica::FiguraGeomtrica(string nome){
+	this->nome = nome;
+}
+
+float FiguraGeometrica::calcularArea()
---
+++ Questao 1/Quadrado.h
@@ -0,0 +1,13 @@
+#ifndef QUADRADO_H
+#define QUADRADO_h
+#include "FiguraGeometrica.h"
+
+class Quadrado : public FiguraGeometrica{
+	private: 
+		int base;
+		int lado;
+
+};
+
+
+#endif // QUADRADO_h
